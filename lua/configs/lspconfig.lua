8a01533ed1a48e39f756787a0dd71a1ce1733162
aristorinjuang <aristorinjuang@gmail.com>
2025-04-16 07:23:41 +0700

    fix: errors on lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -19,14 +19,13 @@ local servers = {
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
--- for _, lsp in ipairs(servers) do
---   lspconfig[lsp].setup {
---     on_attach = nvlsp.on_attach,
---     on_init = nvlsp.on_init,
---     capabilities = nvlsp.capabilities,
---   }
--- end
-vim.lsp.enable(servers)
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
