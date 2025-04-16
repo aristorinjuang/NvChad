d9e0132755208314ea9d6d8eef7731b2b028ac86
aristorinjuang <aristorinjuang@gmail.com>
2025-04-16 07:17:18 +0700

    fix: golangci_lint_ls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -36,17 +36,21 @@ end
 -- }
 
 
+local golangci_config = {
+  cmd = {'golangci-lint-langserver'},
+  filetypes = { 'go', 'gomod' },
+  root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
+  init_options = {
+    command = { "golangci-lint", "run", "--output.json.path=stdout", "--show-stats=false" };
+  }
+};
 if not configs.golangcilsp then
- 	configs.golangcilsp = {
-		default_config = {
-			cmd = {'golangci-lint-langserver'},
-			root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
-			init_options = {
-					command = { "golangci-lint", "run", "--enable-all", "--disable", "lll", "--out-format", "json", "--issues-exit-code=1" };
-			}
-		};
-	}
+  configs.golangcilsp = {
+    default_config = golangci_config
+  }
 end
 lspconfig.golangci_lint_ls.setup {
-	filetypes = {'go','gomod'}
+  filetypes = golangci_config.filetypes,
+  root_dir = golangci_config.root_dir,
+  init_options = golangci_config.init_options
 }
